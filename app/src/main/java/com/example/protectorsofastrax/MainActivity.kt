3f74c60a980a5bc544a87d65327f44aca6046d24
zlatkovnik <zlatkovnik@gmail.com>
2021-06-13 22:46:04 +0200

    Dodate slike

--- app/src/main/java/com/example/protectorsofastrax/MainActivity.kt
+++ app/src/main/java/com/example/protectorsofastrax/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.protectorsofastrax
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var AVATARS_CHILD = "avatars/"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseStorage.getInstance().reference
+            .child(AVATARS_CHILD + Firebase.auth.uid)
+            .downloadUrl
+            .addOnSuccessListener{
+                Glide.with(this).load(it.toString()).into(main_avatar_img)
+            }
+
+        main_logout_btn.setOnClickListener {
+            Firebase.auth.signOut()
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
